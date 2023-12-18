e2d6b90c4c284eeee6e0ef837a59d76c3c8320dc
goriori <vladislav.borisov2010@gmail.com>
2023-12-18 13:23:01 +0300

    Update commander. Fix bugs, add feature

--- src/configs/application.config.js
+++ src/configs/application.config.js
@@ -1,38 +1,46 @@
-import {ENTITIES} from "../settings/index.js";
+
+// const objects = [
+//     {
+//         entity: 'browser-page' || 'application',
+//         name: 'name entity',
+//         href: 'href to page',
+//         path: null || 'path to files .exe'
+//     }
+// ]
 
 export default [
     {
-        entity: ENTITIES[0],
+        entity: 'browser-page',
         name: 'vk',
         href: 'https://vk.com',
         path: null
     },
     {
-        entity: ENTITIES[0],
+        entity: 'browser-page',
         name: 'weeek',
         href: 'https://app.weeek.net/ws/438419',
         path: null
     },
     {
-        entity: ENTITIES[0],
+        entity: 'browser-page',
         name: 'yandex-translate',
         href: 'https://translate.yandex.ru/',
         path: null
     },
     {
-        entity: ENTITIES[0],
+        entity: 'browser-page',
         name: 'figma',
         href: 'https://www.figma.com/files/recents-and-sharing/recently-viewed?fuid=1291360825231734683',
         path: null
     },
     {
-        entity: ENTITIES[1],
+        entity: 'application',
         name: 'web-storm',
         href: null,
         path: 'C:\\Program Files\\JetBrains\\WebStorm 2023.3\\bin\\webstorm64.exe'
     },
     {
-        entity: ENTITIES[1],
+        entity: 'application',
         name: 'telegram',
         href: null,
         path: 'C:\\goriori\\programms\\Telegram Desktop\\Telegram.exe'
