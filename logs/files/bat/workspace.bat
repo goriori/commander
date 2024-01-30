d095caf60363c087c8681c15bf4afc7fccc6c60c
goriori <vladislav.borisov2010@gmail.com>
2024-01-30 09:06:46 +0300

    Update.

---
+++ logs/files/bat/workspace.bat
@@ -0,0 +1,11 @@
+start https://vk.com
+start https://app.weeek.net/ws/438419
+start https://translate.yandex.ru/
+start https://www.figma.com/files/recents-and-sharing/recently-viewed?fuid=1291360825231734683
+start https://www.bybit.com/ru-RU/trade/spot/BTC/USDT
+cd C:\Program Files\JetBrains\WebStorm 2023.3\bin\ 
+start webstorm64.exe
+cd C:\goriori\programms\Telegram Desktop\ 
+start Telegram.exe
+cd C:\goriori\programms\obsidian\ 
+start Obsidian.exe
